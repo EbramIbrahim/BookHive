2a9c27f85f850e8b6a9e0245a732ce696b492f4a
EbramIbrahim <abramibrahem22@gmail.com>
2025-05-16 20:37:44 +0300

    create all books screen

---
+++ app/src/main/java/com/example/bazar/feature/home_screen/presentation/ui/AllBooksScreen.kt
@@ -0,0 +1,182 @@
+package com.example.bazar.feature.home_screen.presentation.ui
+
+import android.content.Context
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Search
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.example.bazar.R
+import com.example.bazar.feature.home_screen.domain.model.Books
+
+
+@Composable
+fun AllCards(books: List<Books>, context: Context) {
+    Scaffold(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White),
+        topBar = {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceAround
+            ) {
+                Text(
+                    text = "All Books",
+                    style = TextStyle(
+                        color = Color.Black,
+                        fontSize = 18.sp,
+                        fontStyle = FontStyle.Italic
+                    )
+                )
+
+                Icon(
+                    Icons.Outlined.Search,
+                    contentDescription = "",
+                )
+            }
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+        ) {
+            HorizontalDivider()
+
+            LazyColumn(
+                verticalArrangement = Arrangement.spacedBy(12.dp)
+            ) {
+                items(books) { book ->
+                    AllBookCardSection(book, context)
+                }
+
+            }
+
+        }
+
+    }
+}
+
+
+@Composable
+fun AllBookCardSection(
+    book: Books,
+    context: Context
+) {
+
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(200.dp),
+        shape = RectangleShape,
+        colors = CardDefaults.cardColors(containerColor = Color.LightGray),
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp)
+        ) {
+            Card(
+                modifier = Modifier
+                    .fillMaxHeight(),
+                shape = RoundedCornerShape(8.dp)
+            ) {
+
+                AsyncImage(
+                    modifier = Modifier.fillMaxSize(),
+                    model = ImageRequest.Builder(context = context)
+                        .data(book.bookImage)
+                        .error(R.drawable.books_error)
+                        .placeholder(R.drawable.books_in_progress)
+                        .crossfade(enable = true)
+                        .build(),
+                    contentDescription = book.id.toString(),
+                    contentScale = ContentScale.FillBounds
+                )
+            }
+
+            Spacer(modifier = Modifier.width(8.dp))
+
+            Column(
+                modifier = Modifier.fillMaxHeight(),
+                verticalArrangement = Arrangement.SpaceEvenly
+            ) {
+
+                Column {
+                    Text(
+                        "The Great Gatsby",
+                        style = TextStyle(
+                            color = Color.Black,
+                            fontSize = 22.sp,
+                            fontWeight = FontWeight.Bold
+                        )
+                    )
+
+                    Text(
+                        "Fitzgerald, F. Scott (Francis Scott)",
+                        style = TextStyle(
+                            color = Color.Black,
+                            fontSize = 16.sp,
+                        )
+                    )
+                }
+
+                Column {
+                    Text(
+                        "English",
+                        style = TextStyle(
+                            color = Color.Black,
+                            fontSize = 22.sp,
+                            fontWeight = FontWeight.Bold
+                        )
+                    )
+
+                    Text(
+                        "First loves -- Fiction",
+                        style = TextStyle(
+                            color = Color.Black,
+                            fontSize = 16.sp,
+                        )
+                    )
+                }
+
+            }
+
+
+        }
+    }
+
+}
+
+
--- app/src/main/java/com/example/bazar/feature/home_screen/presentation/ui/OfferBookSection.kt
+++ app/src/main/java/com/example/bazar/feature/home_screen/presentation/ui/OfferBookSection.kt
@@ -3,10 +3,12 @@ package com.example.bazar.feature.home_screen.presentation.ui
 import android.content.Context
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -36,57 +38,61 @@ fun OfferBookSection(
     context: Context
 ) {
 
-    Row(
+    Box(
         modifier = Modifier
             .fillMaxWidth()
             .height(146.dp)
             .clip(RoundedCornerShape(12.dp))
             .background(offerCardBackground),
-        horizontalArrangement = Arrangement.SpaceBetween
     ) {
-
-        Column(
-            modifier = Modifier.padding(start = 23.dp, top = 24.dp, bottom = 24.dp)
-        ) {
-            Text(
-                text = "Special Offer",
-                style = TextStyle(
-                    color = Color.Black,
-                    fontSize = 24.sp,
-                    fontWeight = FontWeight.Bold
-                )
-            )
-            Spacer(modifier = Modifier.height(2.dp))
-            Text(
-                text = "Discount 25%",
-                style = TextStyle(
-                    color = Color.Black,
-                    fontSize = 16.sp
-                )
-            )
-            Spacer(modifier = Modifier.height(14.dp))
-
-            Button(
-                onClick = {}
-            ) {
-                Text(text = "View Details")
-            }
-        }
-
-        AsyncImage(
-            model = ImageRequest.Builder(context = context)
-                .data(book.bookImage)
-                .error(R.drawable.books_error)
-                .placeholder(R.drawable.books_in_progress)
-                .crossfade(enable = true)
-                .build(),
-            contentDescription = book.id.toString(),
+        Row(
             modifier = Modifier
-                .fillMaxHeight(),
-            contentScale = ContentScale.Fit
-        )
+                .fillMaxSize(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
 
 
+            Column(
+                modifier = Modifier.padding(start = 23.dp, top = 24.dp, bottom = 24.dp)
+            ) {
+                Text(
+                    text = "Special Offer",
+                    style = TextStyle(
+                        color = Color.Black,
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                )
+                Spacer(modifier = Modifier.height(2.dp))
+                Text(
+                    text = "Discount 25%",
+                    style = TextStyle(
+                        color = Color.Black,
+                        fontSize = 16.sp
+                    )
+                )
+                Spacer(modifier = Modifier.height(14.dp))
+
+                Button(
+                    onClick = {}
+                ) {
+                    Text(text = "View Details")
+                }
+            }
+
+            AsyncImage(
+                model = ImageRequest.Builder(context = context)
+                    .data(book.bookImage)
+                    .error(R.drawable.books_error)
+                    .placeholder(R.drawable.books_in_progress)
+                    .crossfade(enable = true)
+                    .build(),
+                contentDescription = book.id.toString(),
+                modifier = Modifier
+                    .fillMaxHeight(),
+                contentScale = ContentScale.Fit
+            )
+        }
     }
 }
 
